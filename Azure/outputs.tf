0a09192ba65f30dfc39a8e8b98a74ee2807875ee
Peterliu1457 <peterliu201047@gmail.com>
2023-04-18 22:22:50 +0800

    Update

---
+++ Azure/outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.default.name
+}
+
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.default.name
+}
---
+++ Azure/terraform.tfvars
@@ -0,0 +1,2 @@
+appId    = "b5d32924-528e-45c4-a6c3-1451ea362c0a"
+password = "hTp8Q~KQnTKs.LFXsw5ELg2Moym~_vBR_nejHaEM"
