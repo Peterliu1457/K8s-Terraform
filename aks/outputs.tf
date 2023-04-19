cb6f55074975e758b3cb7afc388e124f17ce44dd
JimmyChuKaSing <98079682+JimmyChuKaSing@users.noreply.github.com>
2023-04-19 22:23:35 +0800

    latest update

---
+++ aks/outputs.tf
@@ -0,0 +1,16 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "resource_group_name" {
+  description = "Azure resource group name"
+  value       = azurerm_resource_group.default.name
+}
+
+output "kubernetes_cluster_name" {
+  description = "AKS cluster name"
+  value       = azurerm_kubernetes_cluster.default.name
+}
